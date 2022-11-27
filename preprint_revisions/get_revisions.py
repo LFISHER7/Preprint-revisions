0e7f1b1bed6aa8512d82fbfeae53fc5c721e3c4b
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-11-27 22:17:26 +0000

    use new functions to get revisions

--- preprint_revisions/get_revisions.py
+++ preprint_revisions/get_revisions.py
@@ -74,7 +74,6 @@ def get_revision_text(url):
 
     return revision_text
 
-
 def parse_args():
     """
     This function parses arguments
@@ -98,34 +97,17 @@ def main():
     args = parse_args()
     server = args.server
 
-    data = load_json(f"{DATA_DIR}/extracted_{server}.json")
-    dois = get_dois(data)
+    versions_dict = get_versions(server)
 
-    url_data = load_json(f"{DATA_DIR}/{server}_urls.json")
-    urls = get_urls(url_data)
-
-    matches_dict = {}
-
-    for pre_doi in dois:
-        for i in urls:
-            if re.search(pre_doi, i):
-
-                if pre_doi not in matches_dict.keys():
-                    matches_dict[pre_doi] = [i]
-                else:
-                    matches_dict[pre_doi].append(i)
-
+    urls = build_urls(versions_dict)
+    
     text_dict = {}
     print("Extracting revision text...")
 
-    for key, value in tqdm(matches_dict.items()):
-
-        # get revision text for each url with > 1 versions
-        if len(value) > 1:
-            for i in value:
-                # exclude urls that are the first version of the preprint
-                if not i.endswith("v1"):
-                    text_dict[i] = get_revision_text(i)
+    for url in tqdm(urls):
+        # exclude urls that are the first version of the preprint
+        if not url.endswith("v1"):
+            text_dict[url] = get_revision_text(url)
 
     save_to_json(text_dict, f"{DATA_DIR}/revision_dict_{server}.json")
 
